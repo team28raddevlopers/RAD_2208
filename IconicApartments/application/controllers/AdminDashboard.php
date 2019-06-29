05ad963468b1e1979de5bad712a9be6c2ceebd5a
JayaniH <jayani.hewa77@gmail.com>
2019-06-29 15:28:10 +0530

    admin URLs secured

--- IconicApartments/application/controllers/AdminDashboard.php
+++ IconicApartments/application/controllers/AdminDashboard.php
@@ -3,106 +3,187 @@
 class AdminDashboard extends CI_Controller{
 
     public function index(){
-                $notifications =$this->User_model->get_notifications($this->session->userdata('user_id'));
-                $data['username']=$this->session->userdata('username');
-                $data['num'] = count($notifications);
-                $this->load->view('admin/dashboard', $data);
+        $notifications =$this->User_model->get_notifications($this->session->userdata('user_id'));
+        $data['username']=$this->session->userdata('username');
+        $data['num'] = count($notifications);
+        
+        if($this->session->userdata('user_type') == 'admin'){
+            $this->load->view('admin/dashboard', $data);
+        }
+        else{
+            redirect('Main/login');
+        }
     }
 
     public function RegisterRequests(){
 
-                $notifications =$this->User_model->get_notifications($this->session->userdata('user_id'));
-                $data['username']=$this->session->userdata('username');
-                $data['num'] = count($notifications);
+        $notifications =$this->User_model->get_notifications($this->session->userdata('user_id'));
+        $data['username']=$this->session->userdata('username');
+        $data['num'] = count($notifications);
 
-                $this->load->view('admin/header_main',$data);
-                $this->load->view('admin/Buttons');
-                $this->load->view('main/footer');
+        if($this->session->userdata('user_type') == 'admin'){
+            $this->load->view('admin/header_main',$data);
+            $this->load->view('admin/Buttons');
+            $this->load->view('main/footer');
 
-                $data["fetch_data"]= $this->AdminRegistrations->fetch_data_resident();
-                $data2["fetch_data"]= $this->AdminRegistrations->fetch_data_masseur();
-                $data3["fetch_data"]= $this->AdminRegistrations->fetch_data_instructor();
-                $data4["fetch_data"]= $this->AdminRegistrations->fetch_data_coach();
-                $this->load->view('admin/RegisterRequests/resident_users',$data);
-                $this->load->view('admin/RegisterRequests/masseur',$data2);
-                $this->load->view('admin/RegisterRequests/instructor',$data3);
-                $this->load->view('admin/RegisterRequests/coach',$data4);
+            $data["fetch_data"]= $this->AdminRegistrations->fetch_data_resident();
+            $data2["fetch_data"]= $this->AdminRegistrations->fetch_data_masseur();
+            $data3["fetch_data"]= $this->AdminRegistrations->fetch_data_instructor();
+            $data4["fetch_data"]= $this->AdminRegistrations->fetch_data_coach();
+            $this->load->view('admin/RegisterRequests/resident_users',$data);
+            $this->load->view('admin/RegisterRequests/masseur',$data2);
+            $this->load->view('admin/RegisterRequests/instructor',$data3);
+            $this->load->view('admin/RegisterRequests/coach',$data4);
+        }
+        else{
+            redirect('Main/login');
+        }
     }
 
     public function Registered(){
-                $notifications =$this->User_model->get_notifications($this->session->userdata('user_id'));
-                $data['username']=$this->session->userdata('username');
-                // $data['notifications']=$notifications;
-                $data['num'] = count($notifications);
+        $notifications =$this->User_model->get_notifications($this->session->userdata('user_id'));
+        $data['username']=$this->session->userdata('username');
+        $data['num'] = count($notifications);
 
-                $this->load->view('admin/header_main',$data);
-                $this->load->view('admin/Buttons');
-                $this->load->view('main/footer');
+        if($this->session->userdata('user_type') == 'admin'){
+            $this->load->view('admin/header_main',$data);
+            $this->load->view('admin/Buttons');
+            $this->load->view('main/footer');
 
-                $data["fetch_data"]= $this->AdminRegistrations->fetch_data_Register_resident();
-                $data2["fetch_data"]= $this->AdminRegistrations->fetch_data_Register_masseur();
-                $data3["fetch_data"]= $this->AdminRegistrations->fetch_data_Register_instructor();
-                $data4["fetch_data"]= $this->AdminRegistrations->fetch_data_Register_coach();
-                $this->load->view('admin/Registered/resident_users',$data);
-                $this->load->view('admin/Registered/masseur',$data2);
-                $this->load->view('admin/Registered/instructor',$data3);
-                $this->load->view('admin/Registered/coach',$data4);
+            $data["fetch_data"]= $this->AdminRegistrations->fetch_data_Register_resident();
+            $data2["fetch_data"]= $this->AdminRegistrations->fetch_data_Register_masseur();
+            $data3["fetch_data"]= $this->AdminRegistrations->fetch_data_Register_instructor();
+            $data4["fetch_data"]= $this->AdminRegistrations->fetch_data_Register_coach();
+            $this->load->view('admin/Registered/resident_users',$data);
+            $this->load->view('admin/Registered/masseur',$data2);
+            $this->load->view('admin/Registered/instructor',$data3);
+            $this->load->view('admin/Registered/coach',$data4);
 
+        }
+        else{
+            redirect('Main/login');
+        }
     }
 
     public function Reports(){
 
         $notifications =$this->User_model->get_notifications($this->session->userdata('user_id'));
         $data['username']=$this->session->userdata('username');
-        // $data['notifications']=$notifications;
         $data['num'] = count($notifications);
 
-        $this->load->view('admin/header_main',$data);
-        $this->load->view('admin/reports/reports');
-        $this->load->view('main/footer');
+        if($this->session->userdata('user_type') == 'admin'){
+            $this->load->view('admin/header_main',$data);
+            $this->load->view('admin/reports/reports');
+            $this->load->view('main/footer');    
+        }
+        else{
+            redirect('Main/login');
+        }
         
     }
 
     public function registeredResidents(){
-        $this->load->view('admin/header_main');
-        $this->load->view('admin/fotter');
-        $data["fetch_data"]= $this->AdminRegistrations->fetch_data_Register_resident();
-        $this->load->view('admin/Reports/Register/resident',$data);
+
+        $notifications =$this->User_model->get_notifications($this->session->userdata('user_id'));
+        $data['username']=$this->session->userdata('username');
+        $data['num'] = count($notifications);
+
+        if($this->session->userdata('user_type') == 'admin'){
+            $data["fetch_data"]= $this->AdminRegistrations->fetch_data_Register_resident();
+            $this->load->view('admin/header_main',$data);
+            $this->load->view('admin/Reports/Register/resident',$data);
+            $this->load->view('main/footer');
+        }
+        else{
+            redirect('Main/login');
+        }
     }
 
     public function registeredCoaches(){
-        $this->load->view('admin/header_main');
-        $this->load->view('admin/fotter');
-        $data4["fetch_data"]= $this->AdminRegistrations->fetch_data_Register_coach();
-        $this->load->view('admin/Reports/Register/coach',$data4);
+
+        $notifications =$this->User_model->get_notifications($this->session->userdata('user_id'));
+        $data['username']=$this->session->userdata('username');
+        $data['num'] = count($notifications);
+
+        if($this->session->userdata('user_type') == 'admin'){
+            $data4["fetch_data"]= $this->AdminRegistrations->fetch_data_Register_coach();
+
+            $this->load->view('admin/header_main',$data);
+            $this->load->view('admin/Reports/Register/coach',$data4);
+            $this->load->view('main/footer');
+        }
+        else{
+            redirect('Main/login');
+        }
     }
 
     public function registeredInstructors(){
-        $this->load->view('admin/header_main');
-        $this->load->view('admin/fotter');
-        $data3["fetch_data"]= $this->AdminRegistrations->fetch_data_Register_instructor();
-        $this->load->view('admin/Reports/Register/instructor',$data3);
+
+        $notifications =$this->User_model->get_notifications($this->session->userdata('user_id'));
+        $data['username']=$this->session->userdata('username');
+        $data['num'] = count($notifications);
+
+        if($this->session->userdata('user_type') == 'admin'){
+            $data3["fetch_data"]= $this->AdminRegistrations->fetch_data_Register_instructor();
+
+            $this->load->view('admin/header_main',$data);
+            $this->load->view('admin/Reports/Register/instructor',$data3);
+            $this->load->view('main/footer');
+        }
+        else{
+            redirect('Main/login');
+        }
     }
 
     public function registeredMasseur(){
-        $this->load->view('admin/header_main');
-        $this->load->view('admin/fotter');
-        $data2["fetch_data"]= $this->AdminRegistrations->fetch_data_Register_masseur();
-        $this->load->view('admin/Reports/Register/masseur',$data2);
+        $notifications =$this->User_model->get_notifications($this->session->userdata('user_id'));
+        $data['username']=$this->session->userdata('username');
+        $data['num'] = count($notifications);
+
+        if($this->session->userdata('user_type') == 'admin'){
+            $data2["fetch_data"]= $this->AdminRegistrations->fetch_data_Register_masseur();
+
+            $this->load->view('admin/header_main',$data);
+            $this->load->view('admin/Reports/Register/masseur',$data2);
+            $this->load->view('main/footer');
+        }
+        else{
+            redirect('Main/login');
+        }
     }
 
     public function registereRequestsResidents(){
-        $this->load->view('admin/header_main');
-        $this->load->view('admin/fotter');
-        $data["fetch_data"]= $this->AdminRegistrations->fetch_data_resident();
-        $this->load->view('admin/Reports/Requests/resident',$data);
+        $notifications =$this->User_model->get_notifications($this->session->userdata('user_id'));
+        $data['username']=$this->session->userdata('username');
+        $data['num'] = count($notifications);
+
+        if($this->session->userdata('user_type') == 'admin'){
+            $data["fetch_data"]= $this->AdminRegistrations->fetch_data_resident();
+
+            $this->load->view('admin/header_main',$data);
+            $this->load->view('admin/Reports/Requests/resident',$data);
+            $this->load->view('main/footer');
+        }
+        else{
+            redirect('Main/login');
+        }
     }
 
     public function registereRequestsCoaches(){
-        $this->load->view('admin/header_main');
-        $this->load->view('admin/fotter');
-        $data4["fetch_data"]= $this->AdminRegistrations->fetch_data_coach();
-        $this->load->view('admin/Reports/Requests/coach',$data4);
+        $notifications =$this->User_model->get_notifications($this->session->userdata('user_id'));
+        $data['username']=$this->session->userdata('username');
+        $data['num'] = count($notifications);
+
+        if($this->session->userdata('user_type') == 'admin'){
+            $data4["fetch_data"]= $this->AdminRegistrations->fetch_data_coach();
+
+            $this->load->view('admin/header_main',$data);
+            $this->load->view('admin/Reports/Requests/coach',$data4);
+            $this->load->view('main/footer');
+        }
+        else{
+            redirect('Main/login');
+        }
     }
 
                
@@ -110,49 +191,109 @@ class AdminDashboard extends CI_Controller{
                
 
     public function registereRequestsMasseur(){
-        $this->load->view('admin/header_main');
-        $this->load->view('admin/fotter');
-        $data2["fetch_data"]= $this->AdminRegistrations->fetch_data_masseur();
-        $this->load->view('admin/Reports/Requests/masseur',$data2);
+        $notifications =$this->User_model->get_notifications($this->session->userdata('user_id'));
+        $data['username']=$this->session->userdata('username');
+        $data['num'] = count($notifications);
+
+        if($this->session->userdata('user_type') == 'admin'){
+            $data2["fetch_data"]= $this->AdminRegistrations->fetch_data_masseur();
+
+            $this->load->view('admin/header_main',$data);
+            $this->load->view('admin/Reports/Requests/masseur',$data2);
+            $this->load->view('main/footer');
+        }
+        else{
+            redirect('Main/login');
+        }
     }
 
     public function registereRequestsInstructors(){
-        $this->load->view('admin/header_main');
-        $this->load->view('admin/fotter');
-        $data3["fetch_data"]= $this->AdminRegistrations->fetch_data_instructor();
-        $this->load->view('admin/Reports/Requests/instructor',$data3);
+        $notifications =$this->User_model->get_notifications($this->session->userdata('user_id'));
+        $data['username']=$this->session->userdata('username');
+        $data['num'] = count($notifications);
+
+        if($this->session->userdata('user_type') == 'admin'){
+            $data3["fetch_data"]= $this->AdminRegistrations->fetch_data_instructor();
+
+            $this->load->view('admin/header_main',$data);
+            $this->load->view('admin/Reports/Requests/instructor',$data3);
+            $this->load->view('main/footer');
+        }
+        else{
+            redirect('Main/login');
+        }
     }
 
     public function removedInstructors(){
-        $this->load->view('admin/header_main');
-        $this->load->view('admin/fotter');
-        $data3["fetch_data"]= $this->AdminRegistrations->fetch_data_Removed_instructor();
-        $this->load->view('admin/Reports/Removed/instructor',$data3);
+        $notifications =$this->User_model->get_notifications($this->session->userdata('user_id'));
+        $data['username']=$this->session->userdata('username');
+        $data['num'] = count($notifications);
+
+        if($this->session->userdata('user_type') == 'admin'){
+            $data3["fetch_data"]= $this->AdminRegistrations->fetch_data_Removed_instructor();
+
+            $this->load->view('admin/header_main',$data);
+            $this->load->view('admin/Reports/Removed/instructor',$data3);
+            $this->load->view('main/footer');
+        }
+        else{
+            redirect('Main/login');
+        }
     }
 
     
     public function removedResident(){
-        $this->load->view('admin/header_main');
-        $this->load->view('admin/fotter');
-        $data3["fetch_data"]= $this->AdminRegistrations->fetch_data_Removed_resident();
-        $this->load->view('admin/Reports/Removed/resident',$data3);
+        $notifications =$this->User_model->get_notifications($this->session->userdata('user_id'));
+        $data['username']=$this->session->userdata('username');
+        $data['num'] = count($notifications);
+
+        if($this->session->userdata('user_type') == 'admin'){
+            $data3["fetch_data"]= $this->AdminRegistrations->fetch_data_Removed_resident();
+
+            $this->load->view('admin/header_main',$data);
+            $this->load->view('admin/Reports/Removed/resident',$data3);
+            $this->load->view('main/footer');
+        }
+        else{
+            redirect('Main/login');
+        }
     }
 
     public function removedMasseur(){
-        $this->load->view('admin/header_main');
-        $this->load->view('admin/fotter');
-        $data3["fetch_data"]= $this->AdminRegistrations->fetch_data_Removed_masseur();
-        $this->load->view('admin/Reports/Removed/masseur',$data3);
+        $notifications =$this->User_model->get_notifications($this->session->userdata('user_id'));
+        $data['username']=$this->session->userdata('username');
+        $data['num'] = count($notifications);
+
+        if($this->session->userdata('user_type') == 'admin'){
+            $data3["fetch_data"]= $this->AdminRegistrations->fetch_data_Removed_masseur();
+
+            $this->load->view('admin/header_main',$data);
+            $this->load->view('admin/Reports/Removed/masseur',$data3);
+            $this->load->view('admin/fotter');
+        }
+        else{
+            redirect('Main/login');
+        }
     }
 
     public function removedCoach(){
-        $this->load->view('admin/header_main');
-        $this->load->view('admin/fotter');
-        $data3["fetch_data"]= $this->AdminRegistrations->fetch_data_Removed_coach();
-        $this->load->view('admin/Reports/Removed/coach',$data3);
+        $notifications =$this->User_model->get_notifications($this->session->userdata('user_id'));
+        $data['username']=$this->session->userdata('username');
+        $data['num'] = count($notifications);
+
+        if($this->session->userdata('user_type') == 'admin'){
+            $data3["fetch_data"]= $this->AdminRegistrations->fetch_data_Removed_coach();
+
+            $this->load->view('admin/header_main',$data);
+            $this->load->view('admin/Reports/Removed/coach',$data3);
+            $this->load->view('main/footer');
+        }
+        else{
+            redirect('Main/login');
+        }
     }
 
-//controllers to handle bookings
+//functions to handle bookings
 
     public function spa_pending_bookings(){
         if($this->session->userdata('user_type') == 'admin'){
